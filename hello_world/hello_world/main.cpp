ff5edc3218c027bb6b8f55ba4d0235a2c13d6cf2
nkls <niklas@long.ch>
2018-09-15 18:33:11 +0200

    add vars and explanations

--- hello_world/hello_world/main.cpp
+++ hello_world/hello_world/main.cpp
@@ -7,9 +7,23 @@
 //
 
 #include <iostream>
+using namespace std;
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+
+    //    var initialisation is the same for built in types.
+    //    ex: int x(1); is identical to int x = 1; as int is a built in type.
+    int x(1);
+    double y = 2.3;
+    string z("chocolate");
+    string w = "no pain, no gain";
+    
+    std::cout << "x is: " << x << std::endl;
+    std::cout << "y is: " << y << std::endl;
+    std::cout << "z is: " << z << std::endl;
+    std::cout << "w is: " << w << std::endl;
+
+    
+    cout << "Hello, World!\n";
     return 0;
 }
