51dbb5c1805585ad7f11bbc01c7d0c23de008bb1
nkls <niklas@long.ch>
2018-09-15 17:34:31 +0200

    initial setup

---
+++ s/s/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  s
+//
+//  Created by Niklas on 15.09.18.
+//  Copyright © 2018 Niklas. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
