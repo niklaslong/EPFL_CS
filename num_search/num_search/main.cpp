fd70c078c20df53c0f05651b31da6283fea04693
nkls <niklas@long.ch>
2018-09-21 18:03:38 +0200

    add num_search

---
+++ num_search/num_search/main.cpp
@@ -0,0 +1,43 @@
+//
+//  main.cpp
+//  num_search
+//
+//  Created by Niklas on 21.09.18.
+//  Copyright © 2018 Niklas. All rights reserved.
+//
+
+#include <iostream>
+
+using namespace std;
+
+// instruction can be 0: initial, 1: true, 2: false-smaller, 3: false-larger
+
+int main(int argc, const char * argv[]) {
+    cout << "choose a number greater than 2" << endl;
+    
+    int max(20);
+    int min(1);
+    int num(19);
+    int guess;
+    
+    cout << "guessing" << endl;
+    
+    guess = max;
+    
+    cout << "is your number " << guess << "?" << endl;
+    
+    while (num != guess) {
+        if (guess > num) {
+            max = guess;
+            guess /= 2;
+        } else {
+            guess = max - (guess / 2);
+        }
+    }
+    
+    cout << guess << "yay!";
+    
+    return 0;
+}
+
+
