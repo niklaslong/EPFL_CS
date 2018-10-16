8ed1f10adce5d6a3d1dd405085e3de4bacf53ccc
nkls <niklas@long.ch>
2018-10-16 18:04:11 +0200

    add query_num examples

--- week_five/week_five/main.cpp
+++ week_five/week_five/main.cpp
@@ -13,10 +13,14 @@ using namespace std;
 int variable(10);
 
 int scope();
+int query_num(int min, int max);
+int display_num();
+bool validate_input(int min, int max, int num_input);
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    scope();
+//    scope();
+    display_num();
+    
     return 0;
 }
 
@@ -47,5 +51,46 @@ int scope() {
 
     cout << "Six, la variable vaut : " << variable << endl;
     //    As we redifined the global var in the for loop above, it is now (3);
+    return 0;
+}
+
+bool validate_input(int min, int max, int num_input) {
+    bool output;
+    
+    if (max <= min) {
+        if (min <= num_input) { output = true; }
+        else { output = false; }
+    } else {
+        if (min <= num_input && num_input <= max) { output = true; }
+        else { output = false; }
+    }
+    
+    return output;
+}
+
+int query_num(int min, int max) {
+    int num_input;
+    
+    bool input_valid(false);
+    
+    do {
+        cout << "please enter an integer: " << endl;
+        cin >> num_input;
+        
+        input_valid = validate_input(min, max, num_input);
+        
+    } while(!input_valid);
+    
+    return num_input;
+}
+
+int display_num() {
+    int max(7);
+    int min(2);
+    
+    int num = query_num(min, max);
+    
+    cout << "Your num was: " << num;
+    
     return 0;
 }
