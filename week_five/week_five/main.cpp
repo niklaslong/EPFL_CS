b1d8813063648e73c82e940aebe57dae98716271
nkls <niklas@long.ch>
2018-10-16 18:28:56 +0200

    add passing by reference example (exchange)

--- week_five/week_five/main.cpp
+++ week_five/week_five/main.cpp
@@ -21,14 +21,27 @@ bool validate_input(int min, int max, int num_input);
 double min2(double x, double y);
 double min3(double x, double y, double z);
 
+int exchange(int &x, int &y);
+
 
 
 int main(int argc, const char * argv[]) {
 //    scope();
 //    display_num();
     
-    cout << "The smallest is: " << min2(5, 7) << endl;
-    cout << "The smallest is: " << min3(5, 1, 2) << endl;
+//  MIN2 and MIN3
+//    cout << "The smallest is: " << min2(5, 7) << endl;
+//    cout << "The smallest is: " << min3(5, 1, 2) << endl;
+
+//  EXCHANGE
+//    int i(10);
+//    int j(55);
+//
+//    cout << "Avant: i=" << i << " et j=" << j << endl;
+//    exchange(i,j);
+//    cout << "Après: i=" << i << " et j=" << j << endl;
+    
+//    
 
     return 0;
 }
@@ -110,4 +123,14 @@ double min2(double x, double y) {
 
 double min3(double x, double y, double z) {
     return min2(min2(x, y), z);
+}
+
+int exchange(int &x, int &y) {
+//    Don't overuse passing by reference!
+    int tmp(x);
+    
+    x = y;
+    y = tmp;
+    
+    return 0;
 }
