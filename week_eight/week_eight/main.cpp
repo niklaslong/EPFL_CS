031fc019d285ed8167f5475fd5d78e07b3fd3baf
Long Niklas <long@INTRANET.EPFL.CH@CO-IN-SC-235.intranet.epfl.ch>
2018-11-08 18:25:43 +0100

    add begining of split

--- week_eight/week_eight/main.cpp
+++ week_eight/week_eight/main.cpp
@@ -12,10 +12,28 @@
 #include <string>
 
 void generate_letter(bool sex, std::string receptor, std::string subject, std::string date, std::string signoff, std::string author);
+std::string split(std::string string);
+bool next_token(const std::string &str, int from, int len);
 
 int main(int argc, const char * argv[]) {
-    generate_letter(true, "John", "votre demande de rdv" , "12/12", "best", "ME");
+    // generate_letter(true, "John", "votre demande de rdv" , "12/12", "best", "ME");
+    
+    next_token("hello world this is a string", -1, -1);
     return 0;
+}
+
+bool next_token(const std::string &str, int from, int len) {
+	for (std::size_t i(0); i < str.size(); ++i) {
+		if (str[i] == ' ' && from < 0) {
+			
+		}
+	}
+	
+	return true;
+}
+
+std::string split(std::string string) {
+	return "hello world";
 }
 
 void generate_letter(
